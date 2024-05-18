90eb7429cc3090b957ebb576517dc6928e30c35e
Mintu Singh <152206810+MintuSingh07@users.noreply.github.com>
2024-05-18 18:45:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-const host = "http://localhost:8000"
+// const host = "http://localhost:8000"
+const host = "https://fees-management-be.onrender.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
