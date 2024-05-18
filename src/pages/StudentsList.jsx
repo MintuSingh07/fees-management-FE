ba0983733664fb83c906b5e05d4490d360aa4ddb
Mintu Singh <mintusingh2006j@gmail.com>
2024-05-18 10:51:13 +0530

    delete student feature

--- src/pages/StudentsList.jsx
+++ src/pages/StudentsList.jsx
@@ -9,12 +9,15 @@ const StudentsList = () => {
     const [students, setStudents] = useState([]);
     const [date, setDate] = useState('');
     const [searchTerm, setSearchTerm] = useState('');
+    const [confirmDelete, setConfirmDelete] = useState({ isOpen: false, studentId: null, studentName: '' });
 
+    //* Get data from local storage [ADMIN or NOT]
     useEffect(() => {
         const admToken = localStorage.getItem('admToken');
         if (admToken) setIsLoggedIn(true);
     }, []);
 
+    //* Get student data
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -34,6 +37,7 @@ const StudentsList = () => {
         }
     }, [isLoggedIn]);
 
+    //* Get month
     useEffect(() => {
         const currentDate = new Date();
         const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
@@ -42,6 +46,7 @@ const StudentsList = () => {
         setDate(monthAndYear);
     }, []);
 
+    //* Payment Handler
     const handleCheckboxChange = async (uuid, isPaid) => {
         try {
             const response = await axios.put(`http://localhost:8000/update-payment/${uuid}`, { isPaid: !isPaid }, {
@@ -62,13 +67,38 @@ const StudentsList = () => {
         }
     };
 
+    //* Search Students
     const handleSearch = (e) => {
         setSearchTerm(e.target.value);
     };
-
     const filteredStudents = searchTerm.length === 0 ? students : students.filter(student =>
         student.fullName.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    //* Delete Student
+    const handleConfirmDelete = (uuid, fullName) => {
+        setConfirmDelete({ isOpen: true, studentId: uuid, studentName: fullName });
+    };
+
+    const handleCancelDelete = () => {
+        setConfirmDelete({ isOpen: false, studentId: null, studentName: '' });
+    };
+
+    const handleStudentDelete = async () => {
+        try {
+            const response = await axios.delete(`http://localhost:8000/remove/${confirmDelete.studentId}`, {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('admToken')}`
+                }
+            });
+            if (response.status === 200) {
+                setStudents(students.filter(student => student.uuid !== confirmDelete.studentId));
+            }
+            console.log(response.data.message);
+        } catch (error) {
+            console.error("Error removing student:", error.response ? error.response.data.message : error.message);
+        }
+    };
 
     return (
         <div style={{ padding: "2rem" }}>
@@ -77,7 +107,7 @@ const StudentsList = () => {
                     <div id="topHead">
                         <h2>{date}</h2>
                         <Link to="/add-std" id="addStd">
-                            <MdPersonAddAlt1 style={{fontSize: "1.5rem", color: "black"}}/>
+                            <MdPersonAddAlt1 style={{ fontSize: "1.5rem", color: "black" }} />
                         </Link>
                     </div>
                     <input
@@ -87,6 +117,16 @@ const StudentsList = () => {
                         onChange={handleSearch}
                         id='searchBar'
                     />
+                    {confirmDelete.isOpen && (
+                        <div id='dialogue-box'>
+
+                            <div>
+                                <p id='delete-message'>{`Do you want to remove ${confirmDelete.studentName}? Click "OK" to confirm !!!`}</p>
+                                <button onClick={handleStudentDelete}>OK</button>
+                                <button onClick={handleCancelDelete}>Cancel</button>
+                            </div>
+                        </div>
+                    )}
                     {filteredStudents.length > 0 ? (
                         <ul>
                             {filteredStudents.map(student => (
@@ -95,6 +135,7 @@ const StudentsList = () => {
                                     <p>Class: {student.stdClass}</p>
                                     <p>UUID: {student.uuid}</p>
                                     <p>Phone Number: {student.phone}</p>
+                                    <button onClick={() => handleConfirmDelete(student.uuid, student.fullName)}>Delete</button>
                                     {student.isPaid ? (
                                         <p style={{ color: 'green', fontWeight: 600 }}>Paid</p>
                                     ) : (
